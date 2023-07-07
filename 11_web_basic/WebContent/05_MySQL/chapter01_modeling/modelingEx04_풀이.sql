de8b71a27f38071ba372be1a1c4022a6ecb35256
15_web_ysh <15_web_ysh@302-13>
2023-07-07 17:40:33 +0900

    23-07-07

--- 11_web_basic/WebContent/05_MySQL/chapter01_modeling/modelingEx04_풀이.sql
+++ 11_web_basic/WebContent/05_MySQL/chapter01_modeling/modelingEx04_풀이.sql
@@ -2,8 +2,8 @@
     1) BMS_TEMP 데이터베이스를 생성하고 사용하기.
 */	
 
-
-
+CREATE DATABASE BMS_TEMP; 
+USE BMS_TEMP;
 /*    
     2) MEMBER_TB 테이블을 생성하기.
     	
@@ -33,6 +33,32 @@
 		NAMUJIADDRESS 	글자(500)
 		JOIN_DT 		DATE
 */
+CREATE TABLE MEMBER_TB(
+	MEMBER_ID VARCHAR(20),
+    MEMBER_NM VARCHAR(50),
+    PW VARCHAR(1000),
+    GENDER VARCHAR(10),
+    BIRTH_Y VARCHAR(20),
+    BIRTH_M VARCHAR(20),
+    BIRTH_D VARCHAR(20),
+    BIRTH_GN VARCHAR(20),
+    TEL1 VARCHAR(20),
+    TEL2 VARCHAR(20),
+    TEL3 VARCHAR(20),
+    HP1 VARCHAR(20),
+    HP2 VARCHAR(20),
+    HP3 VARCHAR(20),
+    SMSSTS_YN VARCHAR(20),
+    EMAIL1 VARCHAR(20),
+    EMAIL2 VARCHAR(20),
+    EMAILSTS_YN VARCHAR(20),
+    ZIPCODE VARCHAR(20),
+    ROADADDRESS VARCHAR(500),
+    JIBUNADDRESS VARCHAR(20),
+    NAMUJIADDRESS VARCHAR(500),
+    JOIN_DT DATE
+    
+    );
 
 
 
@@ -63,7 +89,29 @@
 		CRE_DATE 		  날짜(일자)
 */
 
+CREATE TABLE GOODS_TB(
+		GOODS_ID INT,
+        TITLE VARCHAR(50),
+        WRITER VARCHAR(50),
+        PRICE INT,
+        PUBLISHER VARCHAR(50),
+        SORT VARCHAR(50),
+        SALES_PRICE INT,
+        POINT INT,
+        PUBLLISHIED_DATE DATE,
+        TOTAL_PAGE INT,
+        ISBN VARCHAR(20),
+        DELIVERY_PRICE INT,
+        DELIVERY_DATE DATE,
+        WRITER_INTRO VARCHAR(2000),
+        CONTENTS_ORDER VARCHAR(2000),
+        INTRO VARCHAR(2000),
+        PUBLISHER_COMMENT VARCHAR(2000),
+        RECOMMENDATION VARCHAR(2000),
+        FILE_NAME VARCHAR(2000),
+        CRE_DATE DATE
 
+);
 
 /*    
     4) ORDER_TB 테이블을 생성하기.
@@ -97,7 +145,29 @@
 		DELIVERY_STATUS 	글자(100)
 */
 
-
+create table order_tb(
+	order_id int.
+    member_id varchar(50),
+    goods_id int,
+    goods_title varchar(100),
+    goods_sales_price int,
+    orderer_name varchar(50),
+    orderer_hp varchar(50),
+    order_goods_qty int,
+    receiver_namer varchar(50),
+    receiver_hp1 varchar(20),
+	receiver_hp2 varchar(20),
+	receiver_hp3 varchar(20),
+    receiver_tel1 varchar(20),
+	receiver_tel2 varchar(20),
+	receiver_tel3 varchar(20),
+    delivery_address varchar(500),
+    delivery_method varchar(40),
+    delivery_message varchar(300),
+    
+    
+    
+);
 
 /*    
     5) MEMBER_TB 테이블을 삭제하기.
---
+++ 11_web_basic/WebContent/05_MySQL/chapter02_sqlBasic/sqlBasicEx06_정답예시.sql
@@ -0,0 +1,210 @@
+CREATE DATABASE DML_PRACTICE;
+USE DML_PRACTICE;       
+
+CREATE TABLE CAR(
+	CAR_CD 			VARCHAR(20),
+	CAR_NM 			VARCHAR(20),
+	PRICE 			INT,
+	BRAND_NM 		VARCHAR(10),
+	CATEGORY		VARCHAR(100),
+	REG_DT			VARCHAR(500)
+);
+
+
+/*
+	
+	CAR테이블에 데이터 삽입하기.
+
+	'car1', '아반떼',  60000, '현대' , '준중형' , '2020-01-01'
+	'car2' , '아이오닉', 67500, '현대', '준중형' , '2020-02-02'
+	'car3' , '쏘나타',  80000, '현대', '중형' , '2020-03-03'
+	'car4' , '그랜저',  115000, '현대', '준대형' , '2020-04-04'
+	'car5' , '스타렉스',  155000, '현대', '대형' , '2020-05-05'
+	'car6' , '제네시스', 175000, '현대', '중형' , '2020-06-06'
+	'car7' , 'k3' , 68000, '기아', '준중형' , '2020-07-07'
+	'car8' , '모닝',  39900, '기아', '소형' , '2020-08-08'
+	'car9' , 'k5', 78000, '기아', '중형' , '2020-09-09'
+	'car10' , 'k7', 115000, '기아', '준대형' , '2020-10-10'
+	'car11', 'k9', 288000, '기아', '대형' , '2020-11-11'
+	'car12', '레이', 65000, '기아', '소형' , '2021-01-01'
+	'car13', '스포티지',  115000, '기아', '준중형' , '2021-02-02'
+	'car14', '카니발', 180000, '기아', '대형' , '2021-03-03'
+	'car15', 'SM5', 50000, '르노', '중형' , '2021-04-04'
+	'car16', 'SM6', 73920, '르노', '중형' , '2021-05-05'
+	'car17', 'QM6', 78000, '르노', '중형' , '2021-06-06'
+	'car18', '렉스턴', 66000, '쌍용', '준대형' , '2021-07-07'
+	'car19', '티볼리', 25500, '쌍용', '소형' , '2021-08-08'
+	'car20', '코란도', 59700, '쌍용', '준중형' , '2021-09-09'
+	
+*/
+
+INSERT INTO CAR VALUES ('car1', '아반떼',  60000, '현대', '준중형' , '2020-01-01');
+INSERT INTO CAR VALUES ('car2' , '아이오닉', 67500, '현대', '준중형' , '2020-02-02');
+INSERT INTO CAR VALUES ('car3' , '쏘나타',  80000, '현대', '중형' , '2020-03-03');
+INSERT INTO CAR VALUES ('car4' , '그랜저',  115000, '현대', '준대형' , '2020-04-04');
+INSERT INTO CAR VALUES ('car5' , '스타렉스',  155000, '현대', '대형' , '2020-05-05');
+INSERT INTO CAR VALUES ('car6' , '제네시스', 175000, '현대', '중형' , '2020-06-06');
+INSERT INTO CAR VALUES ('car7' , 'k3' , 68000, '기아', '준중형' , '2020-07-07');
+INSERT INTO CAR VALUES ('car8' , '모닝',  39900, '기아', '소형' , '2020-08-08');
+INSERT INTO CAR VALUES ('car9' , 'k5', 78000, '기아', '중형' , '2020-09-09');
+INSERT INTO CAR VALUES ('car10' , 'k7', 115000, '기아', '준대형' , '2020-10-10');
+INSERT INTO CAR VALUES ('car11', 'k9', 288000, '기아', '대형' , '2020-11-11');
+INSERT INTO CAR VALUES ('car12', '레이', 65000, '기아', '소형' , '2021-01-01');
+INSERT INTO CAR VALUES ('car13', '스포티지',  115000, '기아', '준중형' , '2021-02-02');
+INSERT INTO CAR VALUES ('car14', '카니발', 180000, '기아', '대형' , '2021-03-03');
+INSERT INTO CAR VALUES ('car15', 'SM5', 50000, '르노', '중형' , '2021-04-04');
+INSERT INTO CAR VALUES ('car16', 'SM6', 73920, '르노', '중형' , '2021-05-05');
+INSERT INTO CAR VALUES ('car17', 'QM6', 78000, '르노', '중형' , '2021-06-06');
+INSERT INTO CAR VALUES ('car18', '렉스턴', 66000, '쌍용', '준대형' , '2021-07-07');
+INSERT INTO CAR VALUES ('car19', '티볼리', 25500, '쌍용', '소형' , '2021-08-08');
+INSERT INTO CAR VALUES ('car20', '코란도', 59700, '쌍용', '준중형' , '2021-09-09');
+
+# 1) 기아 브랜드 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		BRAND_NM = '기아';
+
+        
+# 2) 기아 브랜드가 아닌 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		BRAND_NM <> '기아';
+        
+        
+# 3) 현대와 쌍용 브랜드의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		BRAND_NM IN ('현대' , '쌍용');
+
+
+# 4) 대형차가 아닌 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		CATEGORY <> '대형';
+        
+        
+# 5) 소형차와 대형차의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		CATEGORY IN ('소형' , '대형');
+        
+        
+# 6) 준중형차와 중형차의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		CATEGORY IN ('준중형' , '중형');
+        
+        
+# 7) 중형차중 르노 브랜드의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		CATEGORY = '중형' AND BRAND_NM = '르노';
+        
+        
+# 8) 2021년에 등록된 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		REG_DT BETWEEN '2021-01-01' AND '2021-12-31';
+        
+
+# 9) 렌트비용이 50000 ~ 100000사이 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		PRICE BETWEEN 50000 AND 100000;
+        
+        
+# 10) 기아브랜드 이며 렌트비용이 100000 ~ 200000인 차량의 전체 컬럼 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		BRAND_NM = '기아' AND PRICE BETWEEN 50000 AND 100000;
+        
+        
+# 11) 현대브랜드 차량중 대형차량만 조회하기
+SELECT
+		*
+FROM
+		CAR
+WHERE
+		BRAND_NM = '현대' AND CATEGORY = '대형';
+
+
+# 12) 차량의 전체 컬럼 조회하기 (렌트 가격이 많은 차량 순서)
+SELECT
+		*
+FROM
+		CAR
+ORDER BY
+		PRICE DESC;
+
+
+# 13) 차량의 전체 컬럼 조회하기 (렌트 가격이 적은 차량 순서)
+SELECT
+		*
+FROM
+		CAR
+ORDER BY
+		PRICE;
+
+# 14) 차량의 전체 컬럼 조회하기 (렌트 가격이 적은 차량 순서 , 브랜드 이름이 ㄱ~ㄴ순서 , 차량이름이 ㄱ~ㄴ순서)
+SELECT
+		*
+FROM
+		CAR
+ORDER BY
+		PRICE,
+        BRAND_NM,
+        CAR_NM;
+        
+        
+# 15) 차량의 전체 컬럼 조회하기 (렌트 가격이 많은 차량 순서의 3개의 차량)
+SELECT
+		*
+FROM
+		CAR
+ORDER BY
+		PRICE DESC
+LIMIT
+		3;
+
+
+# 16) 차량의 전체 컬럼 조회하기 (렌트 가격이 적은 차량 순서 , 차량이름이 ㄱ~ㄴ순서의 3개의 차량)
+SELECT
+		*
+FROM
+		CAR
+ORDER BY
+		PRICE,
+        CAR_NM
+LIMIT
+		3;
+
