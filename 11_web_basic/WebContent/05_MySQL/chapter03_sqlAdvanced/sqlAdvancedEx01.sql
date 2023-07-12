b1edb8bb213ab444ca0efb69989f2bff5bd8eba8
15_web_ysh <15_web_ysh@302-13>
2023-07-12 17:38:48 +0900

    23-07-12

---
+++ 11_web_basic/WebContent/05_MySQL/chapter03_sqlAdvanced/sqlAdvancedEx01.sql
@@ -0,0 +1,147 @@
+/*
+
+	# 조인(JOIN) 
+    
+		- 조인은 두 개 이상의 테이블을 결합하는 연산이다.
+		- 서로 다른 테이블에서 데이터를 가져오려면 조인 연산을 해야 한다. 
+		- 주로 주키(PRIMARY KEY)와 참조키(FOREIGN KEY)를 기준으로 테이블을 조인 연산한다.
+        - 대표적인 조인은 INNER , LEFT(LEFT OUTER), RIGHT(RIGHT OUTER) 조인이 있다.
+        - INNER JOIN은 INNER를 생략한 JOIN으로 사용가능하다.        Ex) INNER JOIN , JOIN
+        - LEFT JOIN은 OUTER를 생략한 LEFT JOIN으로 사용가능하다.    Ex) LEFT OUTER JOIN , LEFT JOIN
+        - RIGHT JOIN은 OUTER를 생략한 RIGHT JOIN으로 사용가능하다.  Ex) RIGHT OUTER JOIN , RIGHT JOIN
+ 		- 테이블명은 주로 별칭을 사용한다.
+ 		- 체계적이고 효율적인 설계를 위한 '정규화 과정'을 이해해야 한다.
+   
+		[형식]
+		
+		SELECT
+				*
+		FROM
+				(TABLE1 ALIAS)
+			INNER(LEFT,RIGHT) JOIN (TABLE2 ALIAS)
+								ON (CONDITION)
+
+*/
+
+CREATE DATABASE ADVANCED_EX;
+USE ADVANCED_EX;
+
+CREATE TABLE CLASS (
+	SUBJECT_CD   VARCHAR(20),
+	SUBJECT_NM   VARCHAR(30),
+	LOCATION	 VARCHAR(30)
+);
+
+
+CREATE TABLE STUDENT (
+    STUDENT_CD 	 VARCHAR(10),
+	STUDENT_NM 	 VARCHAR(10),
+	SUBJECT_CD 	 VARCHAR(20)
+);
+
+
+INSERT INTO CLASS VALUES("C1" , "C" , "101호");
+INSERT INTO CLASS VALUES("C2" , "JAVA" , "102호");
+INSERT INTO CLASS VALUES("C3" , "PYTHON" , "201호");
+INSERT INTO CLASS VALUES("C4" , "SPRING" , "202호");
+INSERT INTO CLASS VALUES("C5" , "JSP" , "301호");
+INSERT INTO CLASS VALUES("C6" , "FRONT_END" , "302호");
+
+
+INSERT INTO STUDENT VALUES("S1" , "학생1" , "C1");
+INSERT INTO STUDENT VALUES("S2" , "학생2" , "C2");
+INSERT INTO STUDENT VALUES("S3" , "학생3" , "C3");
+INSERT INTO STUDENT VALUES("S4" , "학생4" , "C4");
+INSERT INTO STUDENT VALUES("S5" , "학생5" , "C4");
+INSERT INTO STUDENT VALUES("S6" , "학생6" , "C4");
+INSERT INTO STUDENT VALUES("S7" , "학생7" , "C5");
+INSERT INTO STUDENT VALUES("S8" , "학생8" , NULL);
+INSERT INTO STUDENT VALUES("S9" , "학생9" , NULL);
+INSERT INTO STUDENT VALUES("S10" , "학생10" , NULL);
+
+SELECT * FROM CLASS;
+SELECT * FROM STUDENT;
+
+
+# WHERE절을 이용한 두 테이블의 join (oracle 조인)
+SELECT 
+		*		# 모든 컬럼 
+        #C.*	# CLASS 테이블의 모든 컬럼 
+        #S.*	# STUDENT테이블의 모든 컬럼 
+FROM 
+		CLASS C, 
+        STUDENT S
+WHERE 
+		C.SUBJECT_CD = S.SUBJECT_CD;
+
+SELECT 
+		C.SUBJECT_CD	AS SUBJECT_CD,
+		C.SUBJECT_NM	AS SUBJECT_NM,
+        S.STUDENT_NM	AS STUDENT_NM
+FROM 
+		CLASS C,
+        STUDENT S
+WHERE 
+		C.SUBJECT_CD = S.SUBJECT_CD;
+
+# INNER JOIN 사용예시  (ANSI 조인)  
+SELECT 
+		*		
+FROM 
+		CLASS C
+	INNER JOIN STUDENT S
+			ON C.SUBJECT_CD = S.SUBJECT_CD;
+
+SELECT 
+		C.SUBJECT_NM	AS SUBJECT_NM,
+        C.LOCATION		AS LOCATION,
+        S.STUDENT_NM	AS STUDENT_NM
+FROM 
+		CLASS C
+	INNER JOIN STUDENT S
+			ON C.SUBJECT_CD = S.SUBJECT_CD;
+		   
+# LEFT 조인 사용 예시
+SELECT 
+		*		
+FROM 
+		CLASS C
+	LEFT JOIN STUDENT S
+			ON C.SUBJECT_CD = S.SUBJECT_CD;
+
+
+# RIGHT 조인 사용 예시
+SELECT 
+		*		
+FROM 
+		CLASS C
+	RIGHT JOIN STUDENT S
+			ON C.SUBJECT_CD = S.SUBJECT_CD;
+
+
+			   	 
+# (연습 예시1) 2층에서 수업을 듣는 학생이름 , 과목이름 , 강의실위치를 조회하시오.			   	 
+SELECT 
+		S.STUDENT_NM AS STUDENT_NM,
+        C.SUBJECT_NM AS SUBJECT_NM,
+        C.LOCATION AS LOCATION
+FROM 
+		CLASS C 
+	INNER JOIN STUDENT S 
+			ON C.SUBJECT_CD = S.SUBJECT_CD
+			#AND C.LOCATION IN ('201호' , '202호');
+WHERE 	
+			C.LOCATION IN ('201호' , '202호');
+
+# (연습 예시2) 각층별로 수업을 듣는 학생수를 조회하고 학생수가 많은 순서대로 조회하시오.
+SELECT 
+		SUBSTRING(C.LOCATION,1,1) AS LAYER,
+        COUNT(*)				  AS STUDENT_CNT 
+FROM 
+		CLASS C 
+	INNER JOIN STUDENT S 
+			ON C.SUBJECT_CD = S.SUBJECT_CD
+GROUP BY 
+		LAYER
+ORDER BY 
+		STUDENT_CNT DESC;
---
+++ 11_web_basic/WebContent/05_MySQL/chapter03_sqlAdvanced/sqlAdvancedEx03_풀이.sql
@@ -0,0 +1,148 @@
+USE ADVANCED_EX;
+CREATE TABLE CAR_TB (
+	CAR_CD 			VARCHAR(20),
+	CAR_NM 			VARCHAR(20),
+	PRICE 			INT,
+	BRAND_NM 		VARCHAR(10),
+	CATEGORY		VARCHAR(30),
+	REG_DT			VARCHAR(20)
+);
+
+CREATE TABLE MEMBER_TB (
+	MEMBER_ID VARCHAR(20),
+	MEMBER_NM VARCHAR(20),
+	CONTACT   VARCHAR(30),
+	SEX		  VARCHAR(1),
+	AGE		  INT
+);
+
+CREATE TABLE RENT_TB (
+	RENT_CD 	VARCHAR(10),
+	RENT_PERIOD INT,
+	CAR_CD 		VARCHAR(10),
+	MEMBER_ID	VARCHAR(10)
+); 
+
+
+INSERT INTO CAR_TB VALUES ('car1', '아반떼',  60000, '현대' , '준중형' , '2020-01-01');
+INSERT INTO CAR_TB VALUES ('car2' , '아이오닉', 67500, '현대', '준중형' , '2020-02-02');
+INSERT INTO CAR_TB VALUES ('car4' , '그랜저',  115000, '현대', '준대형' , '2020-04-04');
+INSERT INTO CAR_TB VALUES ('car5' , '스타렉스',  155000, '현대', '대형' , '2020-05-05');
+INSERT INTO CAR_TB VALUES ('car6' , '제네시스', 175000, '현대', '중형' , '2020-06-06');
+INSERT INTO CAR_TB VALUES ('car7' , 'k3' , 68000, '기아', '준중형' , '2020-07-07');
+INSERT INTO CAR_TB VALUES ('car8' , '모닝',  39900, '기아', '소형' , '2020-08-08');
+INSERT INTO CAR_TB VALUES ('car9' , 'k5', 78000, '기아', '중형' , '2020-09-09');
+INSERT INTO CAR_TB VALUES ('car10' , 'k7', 115000, '기아', '준대형' , '2020-10-10');
+INSERT INTO CAR_TB VALUES ('car12', '레이', 65000, '기아', '소형' , '2021-01-01');
+INSERT INTO CAR_TB VALUES ('car13', '스포티지',  115000, '기아', '준중형' , '2021-02-02');
+INSERT INTO CAR_TB VALUES ('car14', '카니발', 180000, '기아', '대형' , '2021-03-03');
+INSERT INTO CAR_TB VALUES ('car15', 'SM5', 50000, '르노', '중형' , '2021-04-04');
+INSERT INTO CAR_TB VALUES ('car16', 'SM6', 73920, '르노', '중형' , '2021-05-05');
+INSERT INTO CAR_TB VALUES ('car17', 'QM6', 78000, '르노', '중형' , '2021-06-06');
+INSERT INTO CAR_TB VALUES ('car18', '렉스턴', 66000, '쌍용', '준대형' , '2021-07-07');
+INSERT INTO CAR_TB VALUES ('car19', '티볼리', 25500, '쌍용', '소형' , '2021-08-08');
+INSERT INTO CAR_TB VALUES ('car20', '코란도', 59700, '쌍용', '준중형' , '2021-09-09');
+
+INSERT INTO MEMBER_TB VALUES('user1' , '유저1' , '010-1111-1111' , 'f' , 25);
+INSERT INTO MEMBER_TB VALUES('user2' , '유저2' , '010-1111-2222' , 'm' , 21);
+INSERT INTO MEMBER_TB VALUES('user3' , '유저3' , '010-1111-3333' , 'f' , 30);
+INSERT INTO MEMBER_TB VALUES('user4' , '유저4' , '010-1111-4444' , 'm' , 33);
+INSERT INTO MEMBER_TB VALUES('user5' , '유저5' , '010-1111-5555' , 'f' , 27);
+INSERT INTO MEMBER_TB VALUES('user6' , '유저6' , '010-1111-6666' , 'm' , 32);
+INSERT INTO MEMBER_TB VALUES('user7' , '유저7' , '010-1111-7777' , 'f' , 29);
+INSERT INTO MEMBER_TB VALUES('user8' , '유저8' , '010-1111-8888' , 'm' , 38);
+INSERT INTO MEMBER_TB VALUES('user9' , '유저9' , '010-1111-9999' , 'f' , 22);
+INSERT INTO MEMBER_TB VALUES('user10' , '유저10' , '010-1111-0000' , 'm' , 35);
+
+INSERT INTO RENT_TB VALUES('rent1' , 3 , 'car1' , 'user1');
+INSERT INTO RENT_TB VALUES('rent2' , 1 , 'car1' , 'user9');
+INSERT INTO RENT_TB VALUES('rent3' , 5 , 'car19' , 'user3');
+INSERT INTO RENT_TB VALUES('rent4' , 1 , 'car2' , 'user5');
+INSERT INTO RENT_TB VALUES('rent5' , 2 , 'car15' , 'user6');
+INSERT INTO RENT_TB VALUES('rent6' , 1 , 'car2' , 'user7');
+INSERT INTO RENT_TB VALUES('rent7' , 1 , 'car5' , 'user2');
+INSERT INTO RENT_TB VALUES('rent8' , 1 , 'car7' , 'user8');
+INSERT INTO RENT_TB VALUES('rent9' , 1 , 'car3' , 'user10');
+INSERT INTO RENT_TB VALUES('rent10' , 2 , 'car9' , 'user1');
+INSERT INTO RENT_TB VALUES('rent11' , 3 , 'car20' , 'user1');
+INSERT INTO RENT_TB VALUES('rent12' ,  1 , 'car7' , 'user8');
+INSERT INTO RENT_TB VALUES('rent13' ,  5 , 'car6' , 'user2');
+INSERT INTO RENT_TB VALUES('rent14' , 1 , 'car8' , 'user6');
+INSERT INTO RENT_TB VALUES('rent15' ,  2 , 'car9' , 'user7');
+INSERT INTO RENT_TB VALUES('rent16' ,  3 , 'car1' , 'user3');
+INSERT INTO RENT_TB VALUES('rent17' , 1 , 'car12' , 'user1');
+INSERT INTO RENT_TB VALUES('rent18' , 7 , 'car12' , 'user3');
+INSERT INTO RENT_TB VALUES('rent19' , 1 , 'car4' , 'user3');
+INSERT INTO RENT_TB VALUES('rent20' , 2 , 'car16' , 'user5');
+
+
+# 1) 렌트일이 5일 이상인 차량정보 조회하기
+
+        
+        
+# 2) 렌트일이 5일 이상인 회원정보 조회하기
+
+
+
+# 3) '스타렉스'차량을 렌트한 회원정보를 조회하기
+
+
+
+# 4) 'k3' , 'k5' , 'k7'을 주문한 회원정보를 조회하기
+
+          
+          
+# 5) '아반떼' 차량의 평균 렌트일 조회하기
+
+
+          
+# 6) '유저1'의 렌트 횟수 조회하기
+
+        
+        
+# 7) 남성의 평균 렌트일 조회하기
+
+
+
+# 8) 20대 여성의 렌트일 조회하기
+
+
+         
+# 9) 50000 ~ 100000 가격의 차량의 렌트횟수를 조회하기
+
+  
+
+# 10) 100000이상 가격의 차량의 렌트횟수를 조회하기
+
+         
+         
+# 11) 2020년에 등록된 차량의 렌트 횟수를 조회하기
+
+         
+
+# 12) 기아 자동차의 렌트횟수를 조회하기
+
+      
+ 
+# 13) 브랜드별로 렌트 횟수를 조회하고 렌트수가 많은 순서대로 정렬하기
+
+	
+        
+# 14) 차량이 등록된 연도별로 렌트 횟수를 조회하기
+
+
+
+# 15) 차량이 등록된 연도별로 성별로 렌트 횟수를 조회하기
+
+       
+
+# 16) 차량이 등록된 연도별로 성별로 평균 렌트일을 조회하기
+        
+     
+
+# 17) 가장 많이 렌트된 차량의 이름과 렌트 횟수를 조회하기
+		
+
+
+# 18) 가장 많이 렌트한 회원의 아이디 , 연락처 , 렌트횟수를 조회하기
+
